e6613878c47d59d34a69661b62c71ec7a16fc167
sakar <sakar@gmail.com>
2022-05-24 18:49:17 +0300

    toze extra stuff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById('root'));
+ReactDOM.render(
+<div>
+    <h1>Hello everyone!</h1>
+    <h5>this is some other extra text right over here :)</h5>
+    <p>
+        why did we put all of them inside a div?
+        because the thing we wanna render should be only one thing not more than one 
+    </p>
+</div>
+, document.getElementById('root'));
