584f22772554e05f18bc540019649d30fd6c191d
sakar <sakar@gmail.com>
2022-05-25 16:05:08 +0300

    adding list items

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 ReactDOM.render(
 <div>
-    <h1>Hello everyone!</h1>
-    <h5>this is some other extra text right over here :)</h5>
-    <p>
-        why did we put all of them inside a div?
-        because the thing we wanna render should be only one thing not more than one 
-    </p>
+    <ul>
+        <li>this is first li</li>
+        <li>this is second li</li>
+    </ul>
 </div>
 , document.getElementById('root'));
